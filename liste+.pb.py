4ff90d1d0f2cb25bc73eb1a97b51b2b9317da67f
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2021-01-21 17:48:44 +0200

    Delete liste+.pb.py

--- liste+.pb.py
+++
@@ -1,8 +0,0 @@
-  
-with open ('fisier.liste.txt', 'r') as f:
-    x=f.write()
-y=sorted(x
-print('Lista 5=',x,'ord.crescator=',y,'ord.descresc=',x.sort(reverse=True),'lungimea sirului=',len(x),'lista modificata=',x.extend([111]),'lista modificata .2=',x.insert(2,222),end =('\n'))
-
-  
-    
